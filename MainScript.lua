6c3c984fef065c49a450a9638894fb641056258d
NoobExploits <142132589+NoobExploits@users.noreply.github.com>
2024-01-06 15:35:28 -0600

    idk how to use universal ids

--- MainScript.lua
+++ MainScript.lua
@@ -1,6 +1,6 @@
 local PlaceID = game.PlaceId
 
-if PlaceID == 73885730 then
+if PlaceID == 155615604 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NoobExploits/Scripts-ROBLOX/main/scripts/PrisonLife.lua"))()
 elseif PlaceID == 1 then
     loadstring(game:HttpGet(""))()
