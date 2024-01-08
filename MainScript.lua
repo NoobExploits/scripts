6e06bda927550e051a066553c6b4cad0ee8535d7
NoobExploits <142132589+NoobExploits@users.noreply.github.com>
2024-01-07 20:56:50 -0600

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -2,6 +2,6 @@ local PlaceID = game.PlaceId
 
 if PlaceID == 155615604 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NoobExploits/Scripts-ROBLOX/main/scripts/PrisonLife.lua"))()
-elseif PlaceID == 1 then
-    loadstring(game:HttpGet(""))()
+elseif PlaceID == 662417684 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NoobExploits/Scripts-ROBLOX/main/scripts/Lucky%20Blocks.lua"))()
 end
