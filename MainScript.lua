da40c970fea54344695342875b829a2ce5c41620
NoobExploits <142132589+NoobExploits@users.noreply.github.com>
2024-01-13 00:11:36 -0600

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,7 +1,11 @@
-local PlaceID = game.PlaceId
+local games = {
+    [{155615604}] = "https://raw.githubusercontent.com/NoobExploits/Scripts-ROBLOX/main/scripts/PrisonLife.lua",
+    [{662417684}] = "https://raw.githubusercontent.com/NoobExploits/Scripts-ROBLOX/main/scripts/Lucky%20Blocks.lua",
+}
 
-if PlaceID == 155615604 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NoobExploits/Scripts-ROBLOX/main/scripts/PrisonLife.lua"))()
-elseif PlaceID == 662417684 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NoobExploits/Scripts-ROBLOX/main/scripts/Lucky%20Blocks.lua"))()
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))()
+        break
+    end
 end
